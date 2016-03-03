4099a6a1d4193b8cd6068383da20d845e864dd25
grey927 <zengh927@163.com>
2016-03-04 00:18:41 +0800

    add a function
    add a new function

--- src/printhelloworld/App.java
+++ src/printhelloworld/App.java
@@ -1,7 +1,11 @@
 package printhelloworld;
 
 public class App {
+	
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+	}
+	public static void println(String string) {
+		System.out.println(string);
 	}
 }
